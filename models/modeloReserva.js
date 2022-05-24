2ad2f0cbd3394952ab7730a9e5fe6dfd9cf0f2a1
svelezpernett <saniagovp16@gmail.com>
2022-05-23 21:37:39 -0500

    commit inicial

---
+++ models/modeloReserva.js
@@ -0,0 +1,36 @@
+//Importar mongoose
+import mongoose from 'mongoose'
+
+//ESQUMA
+const Schema = mongoose.Schema //Esquema de datos (QUE DATOS TIENE MI MODELO)
+
+//Creo mi propio esquema de datos
+const Reserva=new Schema({
+    nombre_huesped:{
+        type:String,
+        required:true
+    },
+    contacto_huesped:{
+        type:String,
+        required:true
+    },
+    fecha_inicio:{
+        type:Date,
+        required:true
+    },
+    fecha_final:{
+        type:Date,
+        required:true
+    },
+    nombre_habitacion:{
+        type:String,
+        required:true
+    },
+    cantidad_personas:{
+        type:Number,
+        required:true
+    }
+})
+
+//SE ENVIA EL MODELO DE DATOS
+export const modeloReserva=mongoose.model('reservas', Reserva)
